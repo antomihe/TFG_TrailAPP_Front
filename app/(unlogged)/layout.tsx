328031df488cd9e07744554cb1e970907ba23db7
Antonio Miguel Herrero <antomihe@gmail.com>
2024-10-29 05:01:38 +0100

    TT.11.2 Añadir función de scroll en layout unlogged

--- app/(unlogged)/layout.tsx
+++ app/(unlogged)/layout.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Footer, Header } from '@/components/layout';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Footer } from '@/components/layout/footer';
+import { Header } from '@/components/layout/header';
 
 export default function UnloggedLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto h-screen flex flex-col px-4 md:px-6 lg:px-8">
-      <Header />
+    <div className="h-screen w-screen flex flex-col mx-auto px-3 py-2">
+      <ScrollArea className="flex flex-col overflow-y-auto flex-grow w-full mx-auto px-8 md:px-16">
+        <div className="w-full">
+          <Header />
 
-      <main className="flex-grow flex items-center justify-center">
-        {children}
-      </main>
+          <main className="flex-grow flex items-center justify-center w-full min-h-[calc(100vh-160px)]">
+            {children}
+          </main>
 
-      <Footer />
+          <Footer />
+        </div>
+      </ScrollArea>
     </div>
-  )
+  );
 }
+
