4f3d9ae665739f079bb5e979ed6a9e4e21b6234d
Antonio Miguel Herrero <antomihe@gmail.com>
2024-12-16 00:37:52 +0100

    T.2.2.2.4 Creación interfaz gráfica inscripciones de evento

---
+++ app/(logged)/dashboard/(organizer)/events/enrollments/[eventId]/components/EnrollmentsList.tsx
@@ -0,0 +1,115 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import { useUserState } from '@/store/user/user.store';
+import { Input, Large, Skeleton } from '@/components/ui';
+import api from '@/config/api';
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { CheckIcon, TrashIcon } from 'lucide-react';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { useParams } from 'next/navigation';
+
+interface Enrollment {
+    dorsal: number;
+    name: string;
+    birthdate: string;
+    dni: string;
+    distance: number;
+}
+
+const SkeletonLoader = () => (
+    <div className="p-4">
+        <Skeleton height="h-20" width="w-full" className="my-4" />
+    </div>
+);
+
+export default function EnrollmentList() {
+    const user = useUserState().user;
+    const [enrollments, setEnrollments] = useState<Enrollment[]>([]);
+    const [filteredEnrollments, setFilteredEnrollments] = useState<Enrollment[]>([]);
+    const [loading, setLoading] = useState(true);
+    const { eventId } = useParams();
+
+    useEffect(() => {
+        const fetchEnrollment = async () => {
+            setLoading(true);
+            try {
+                const res = await api(user.access_token).get(`events/enroll/event/${eventId}/list`);
+                setEnrollments(res.data);
+                setFilteredEnrollments(res.data);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchEnrollment();
+    }, [user.id, eventId]);
+
+    const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.toLowerCase();
+        if (value === '') {
+            setFilteredEnrollments(enrollments);
+        } else {
+            setFilteredEnrollments(
+                enrollments.filter(
+                    (enrollment) =>
+                        enrollment.name.toLowerCase().includes(value) ||
+                        enrollment.dorsal.toString().includes(value)
+                )
+            );
+        }
+    };
+
+
+
+    if (loading) {
+        return <SkeletonLoader />;
+    }
+
+    if (enrollments.length === 0) {
+        return <Large className="text-center pt-5">No hay inscripciones para este evento</Large>;
+    }
+
+    return (
+        <div className="max-w-4xl px-4 mx-auto">
+            <div className="flex justify-between items-center mb-4">
+                <Input
+                    type="text"
+                    placeholder="Filtrar por nombre o dorsal "
+                    className="p-2 bg-input border rounded w-2/5"
+                    onChange={handleFilterChange}
+                />
+            </div>
+
+            <div className="mx-auto overflow-x-auto">
+                <ScrollArea className="h-[500px] overflow-y-auto">
+                    <Table className="min-w-full">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Dorsal</TableHead>
+                                <TableHead>Nombre</TableHead>
+                                <TableHead>DNI</TableHead>
+                                <TableHead>Fecha nacimiento</TableHead>
+                                <TableHead>Distancia</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {filteredEnrollments.map((athlete) => (
+                                <TableRow key={athlete.dorsal}>
+                                    <TableCell>{athlete.dorsal}</TableCell>
+                                    <TableCell>{athlete.name}</TableCell>
+                                    <TableCell>{athlete.dni}</TableCell>
+                                    <TableCell>{athlete.birthdate}</TableCell>
+                                    <TableCell>{athlete.distance}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </ScrollArea>
+            </div>
+        </div>
+    );
+}
